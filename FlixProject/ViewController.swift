3ec760c1132f9674e9291646140251fff2b433c5
JesusOcegueraPena <jo1138241@hotmail.com>
2020-01-30 18:24:16 -0800

    Initial Commit

---
+++ FlixProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlixProject
+//
+//  Created by JESUS OCEGUERA-PENA on 1/30/20.
+//  Copyright © 2020 JESUS OCEGUERA-PENA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
