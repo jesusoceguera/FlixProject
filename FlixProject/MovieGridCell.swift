60f4b0f1af5f0c3081a9a9c315a9d7cb3d3a1f44
JesusOcegueraPena <jo1138241@hotmail.com>
2020-02-13 18:14:49 -0800

    Part 2 of flex assignment adding details screen and passing data with also a collection view layout

---
+++ FlixProject/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  FlixProject
+//
+//  Created by JESUS OCEGUERA-PENA on 2/6/20.
+//  Copyright © 2020 JESUS OCEGUERA-PENA. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
